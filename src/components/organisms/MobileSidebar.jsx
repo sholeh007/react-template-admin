2e710fbe98924eb34a2d7f34c9cdf5d5b4400b0e
sholeh007 <sholeh09@outlook.com>
2023-02-18 07:50:32 +0900

    adding nested list on mobile-sidebar

--- src/components/organisms/MobileSidebar.jsx
+++ src/components/organisms/MobileSidebar.jsx
@@ -4,11 +4,15 @@ import {
   DrawerCloseButton,
   DrawerContent,
   DrawerOverlay,
+  Link,
 } from "@chakra-ui/react"
+import { Folder } from "lucide-react"
+import { Link as ReachLink } from "react-router-dom"
 import { lists } from "../../data/list-sidebar"
 import Logo from "../atoms/Logo"
 import List from "../molecules/menu-sidebar/List"
 import ListItem from "../molecules/menu-sidebar/ListItem"
+import NestedList from "../molecules/nested-list/NestedList"
 
 export default function MobileSidebar({ onClose, isOpen }) {
   return (
@@ -30,6 +34,28 @@ export default function MobileSidebar({ onClose, isOpen }) {
                 onClick={onClose}
               />
             ))}
+            <NestedList text="Pages" icon={Folder}>
+              <Link
+                as={ReachLink}
+                to="/login"
+                rounded="lg"
+                p={2}
+                onClick={onClose}
+                _hover={{ textDecor: "none", bg: "bg-color-second" }}
+              >
+                Login
+              </Link>
+              <Link
+                as={ReachLink}
+                to="/404"
+                rounded="lg"
+                p={2}
+                onClick={onClose}
+                _hover={{ textDecor: "none", bg: "bg-color-second" }}
+              >
+                404
+              </Link>
+            </NestedList>
           </List>
         </Box>
       </DrawerContent>
